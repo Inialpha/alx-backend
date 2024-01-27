8d5a3d4e34b6ae19426893053fe7c360bd106cb6
Initech <inimfonebong001@gmail.com>
2024-01-27 11:06:42 +0000

    add annotation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,7 +5,7 @@ import math
 from typing import List
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """ return a tuple of size two containing a
     start index and an end index """
     return ((page - 1) * page_size, page_size * page)
