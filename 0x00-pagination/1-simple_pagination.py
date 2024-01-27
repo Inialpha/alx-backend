6c638cdc429b989ea72bc0ad85e9b5c0999ca3d0
Initech <inimfonebong001@gmail.com>
2024-01-27 11:21:28 +0000

    Add Tuple

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,10 +2,10 @@
 """ Simple pagination """
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ return a tuple of size two containing a
     start index and an end index """
     return ((page - 1) * page_size, page_size * page)
