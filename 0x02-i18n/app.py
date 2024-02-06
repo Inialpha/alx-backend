37f5c8ab65a906963d42c479c49e8403849849f3
Initech <inimfonebong001@gmail.com>
2024-02-06 18:46:52 +0000

    Add task 8

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+""" a simple flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, format_datetime
+from typing import Union, Dict
+import pytz
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """ configure bael """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ return the best match """
+    locale = request.args.get('locale')
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    if g.user and g.user.get('locale') in app.config["LANGUAGES"]:
+        return g.user.get('locale')
+    locale = request.headers.get('locale')
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """ get_timezone function """
+    timezone = request.args.get('timezone')
+    if not timezone and g.user:
+        timezone = g.user.get('timezone')
+    try:
+        return pytz.timezone(timezone).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+def get_user() -> Union[Dict, None]:
+    """ get_user function """
+    user = request.args.get('login_as')
+    if user:
+        user = users.get(int(user))
+    return user
+
+
+@app.before_request
+def before_request() -> None:
+    """ before_request function """
+    user = get_user()
+    g.user = user
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ an index route """
+    g.time = format_datetime()
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
